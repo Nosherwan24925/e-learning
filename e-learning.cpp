b05ddeb4bb1a1b55a9e9b9f845513e3cdb6d28f9
Nosherwan <nosherwan24925@gmail.com>
2024-02-01 11:52:23 +0500

    e-learning file is create

---
+++ e-learning.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+
+class E-Learning()
+{
+
+private:
+Course courses;
+
+
+public:
+
+void Login(String user_name,String user_email);
+void create_course();
+void update_course();
+void orginize_course();
+
+void search_course(String course_id);
+
+
+}
+
+
+
+int main()
+{
+
+    return 0;
+}
